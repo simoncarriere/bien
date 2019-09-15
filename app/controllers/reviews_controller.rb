b278d82c692c08bd4e2952e2845e076662eab4aa
simoncarriere <simoncarriere@icloud.com>
2019-09-15 12:24:16 +0545

    Added Ambiance and SEO

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,8 @@
 class ReviewsController < ApplicationController
 
-
   def index
     # this is our list page for our reviews
     @reviews = Review.all
-
   end
 
 
@@ -15,14 +13,18 @@ class ReviewsController < ApplicationController
 
 
   def create
-    # take info from forma nd add to db
+    # take info from forma nd add to model
     @review = Review.new(forms_params)
 
-    #save this to db
-    @review.save
-
-    #redirect back to homepage
-    redirect_to root_path
+    # we want to check if the model can be saved based on params
+    # if it is, we're going to the home page again
+    # if not, show the new form
+    if @review.save
+      redirect_to root_path
+    else
+      #show the view for new
+      render "new"
+    end
   end
 
 
@@ -35,38 +37,34 @@ class ReviewsController < ApplicationController
   def destroy
     # find the individual review
     @review = Review.find(params[:id])
-
     #destroy
     @review.destroy
-
     # refirect to the home page
     redirect_to root_path
   end
 
 
   def edit
-    #fnd the inidividual page
+    #find the inidividual page
     @review = Review.find(params[:id])
-
   end
 
 
   def update
     #find the inidifual page
     @review = Review.find(params[:id])
-
-
     #update with the new info
-    @review.update(forms_params)
-
-    #redirect to review
-    redirect_to review_path(@review)
-
+    if @review.update(forms_params)
+      #redirect to review
+      redirect_to review_path(@review)
+    else
+      render "edit"
+    end
   end
 
 
   def forms_params
-    params.require(:review).permit(:title, :body, :score)
+    params.require(:review).permit(:title, :restaurant, :body, :score, :ambiance)
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_124155) do
+ActiveRecord::Schema.define(version: 2019_09_04_103105) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_09_03_124155) do
     t.string "cuisine"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "phone_number"
+    t.string "ambiance"
   end
 
 end
